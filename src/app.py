72f37a5f7909d3e816816356f115aa1555bddc47
bk <briceallen@gmail.com>
2022-07-28 21:40:16 -0600

    added altair and numpy
    Signed-off-by: bk <briceallen@gmail.com>

--- src/app.py
+++ src/app.py
@@ -43,14 +43,11 @@
 
 import streamlit as st
 st.set_page_config(page_title="Song Recommendation", layout="wide")
+
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import plotly.express as px
 import streamlit.components.v1 as components
-import altair as alt
-import numpy as np
-from PIL import Image
-
 
 @st.cache(allow_output_mutation=True)
 def load_data():
